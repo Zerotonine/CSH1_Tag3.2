70cf937a7d1665617e6b523463fce6fa8e45d052
Zerotonine <52973359+Zerotonine@users.noreply.github.com>
2020-10-29 15:04:04 +0100

    Globalization.....

--- CSH1_Tag_03.2_Aufgabe_03/CSH1_Tag_03.2_Aufgabe_03/Program.cs
+++ CSH1_Tag_03.2_Aufgabe_03/CSH1_Tag_03.2_Aufgabe_03/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,47 +24,7 @@ namespace CSH1_Tag_03._2_Aufgabe_03
             if (DateTime.TryParse(datumZahl, out datum))
             {
                 datumText = "";
-                string monat = "";
-                switch (datum.Month)
-                {
-                    case 1:
-                        monat = "Januar";
-                        break;
-                    case 2:
-                        monat = "Februar";
-                        break;
-                    case 3:
-                        monat = "März";
-                        break;
-                    case 4:
-                        monat = "April";
-                        break;
-                    case 5:
-                        monat = "Mai";
-                        break;
-                    case 6:
-                        monat = "Juni";
-                        break;
-                    case 7:
-                        monat = "Juli";
-                        break;
-                    case 8:
-                        monat = "August";
-                        break;
-                    case 9:
-                        monat = "September";
-                        break;
-                    case 10:
-                        monat = "Oktober";
-                        break;
-                    case 11:
-                        monat = "November";
-                        break;
-                    case 12:
-                        monat = "Dezember";
-                        break;
-                }
-                datumText += datum.Day + "." + monat + " " + datum.Year;
+                datumText += datum.Day + "." + CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(datum.Month) + " " + datum.Year;
                 return true;
             }
             datumText = null;
